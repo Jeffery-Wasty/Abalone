50f396ee0c26a6dd99136549046d58c12290fbd6
Sam Tadey <39717807+samtadey@users.noreply.github.com>
2019-03-12 14:10:21 -0700

    board, movement

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -9,6 +9,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 
 
 
@@ -19,19 +21,22 @@ import javafx.scene.input.MouseEvent;
  * @author Sam
  * @version 1.0
  */
-public class GUI extends Application {
-    //private HashMap<Integer, Circle> board = new HashMap<Integer, Circle>();
+public class GameBoard extends Group {
+    //
     private Circle[] board = new Circle[BOARDSIZE];
+    //
     private Circle[] pieces = new Circle[BOARDSIZE];
     
     private Circle firstClick;
+    //
     private Circle secondClick;
+    //
     private boolean first = true;
-    /**
-     * The dimensions of my scene.
-     */
+    
+    //
     static final int SCENEBARRIER = 1000;
     
+    //
     static final int BOARDSIZE = 61;
     
     //IF YOU WANT TO MOVE THE BOARD -- this will shift the entire board on the x-axis
@@ -40,61 +45,33 @@ public class GUI extends Application {
     //SHIFT THE WHOLE BOARD IN THE Y-AXIS
     static final int BOARD_START_POSY = 110;
     
-    GameBoard abalone = new GameBoard();
-    
-    
-    /* (non-Javadoc)
-     * @see javafx.application.Application#start(javafx.stage.Stage)
-     */
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-
-        /*Group circlename = buildBoard();
-        Group pieceLayout = standardLayout();
-
-        circlename.getChildren().add(pieceLayout);*/
- 
-        Scene myScene = new Scene(abalone, SCENEBARRIER, SCENEBARRIER);
-        
-        primaryStage.setTitle("Abalone");
-        primaryStage.setScene(myScene);
-        primaryStage.show();
-
+    public GameBoard() {
+        buildBoard();
+        standardLayout();
+        setOnMousePressed(this::processMousePressed);
     }
     
-    /*
-    private void setOnMousePressed(Object object) {
-        //Object target = object.getTarget();
-        if (first) {
-            firstClick = (Circle) object;
-            first = !first;
-        } else {
-            secondClick = (Circle) object;
-            firstClick.setCenterX(secondClick.getCenterX());
-            firstClick.setCenterY(secondClick.getCenterY());
-        }
-    }
-
    
-    
-    
    public void processMousePressed(MouseEvent event) {
-       Object target = event.getTarget();
-       if (first) {
+       Circle target = (Circle) event.getTarget();
+       if (first && target.getFill() != Color.BROWN) {
            firstClick = (Circle) target;
            first = !first;
        } else {
            secondClick = (Circle) target;
            firstClick.setCenterX(secondClick.getCenterX());
            firstClick.setCenterY(secondClick.getCenterY());
+           secondClick = null;
+           firstClick = null;
+           first = !first;
        }
        
    }
     
-    public Group buildBoard () {
+    public void buildBoard () {
       //Initiating board
         
-        Group circlename = new Group();
+        //Group circlename = new Group();
         
         //first (top) row
         // 0 - 4
@@ -106,7 +83,8 @@ public class GUI extends Application {
         {
             Circle c = new Circle(tileshift+=40, posy, rad, Color.BROWN);
             board[i] = c;
-            circlename.getChildren().add(c);
+            getChildren().add(c);
+            //circlename.getChildren().add(c);
         }
         
         //second row
@@ -117,9 +95,9 @@ public class GUI extends Application {
         for (int i = 5; i < 11; i++) {
             Circle c = new Circle(tileshift+=40, posy, rad, Color.BROWN);
             board[i] = c;
-            circlename.getChildren().add(c);
+            getChildren().add(c); 
         }
-            
+        
         //third row
         // 11 - 17
         posx = posx - 20;
@@ -128,7 +106,7 @@ public class GUI extends Application {
         for (int i = 11; i < 18; i++) {
             Circle c = new Circle(tileshift+=40, posy, rad, Color.BROWN);
             board[i] = c;
-            circlename.getChildren().add(c);
+            getChildren().add(c);
         }
         
         //fourth row
@@ -139,7 +117,7 @@ public class GUI extends Application {
         for (int i = 18; i < 26; i++) {
             Circle c = new Circle(tileshift+=40, posy, rad, Color.BROWN);
             board[i] = c;
-            circlename.getChildren().add(c);
+            getChildren().add(c);
         
         }
         
@@ -151,7 +129,7 @@ public class GUI extends Application {
         for (int i = 26; i < 35; i++) {
             Circle c = new Circle(tileshift+=40, posy, rad, Color.BROWN);
             board[i] = c;
-            circlename.getChildren().add(c);
+            getChildren().add(c);
         }
         
         //sixth row
@@ -162,7 +140,7 @@ public class GUI extends Application {
         for (int i = 35; i < 43; i++) {
             Circle c = new Circle(tileshift+=40, posy, rad, Color.BROWN);
             board[i] = c;
-            circlename.getChildren().add(c);
+            getChildren().add(c);
         }
         
         //seventh row
@@ -173,7 +151,7 @@ public class GUI extends Application {
         for (int i = 43; i < 50; i++) {
             Circle c = new Circle(tileshift+=40, posy, rad, Color.BROWN);
             board[i] = c;
-            circlename.getChildren().add(c);
+            getChildren().add(c);
         }
         
         //eighth row
@@ -184,7 +162,7 @@ public class GUI extends Application {
         for (int i = 50; i < 56; i++) {
             Circle c = new Circle(tileshift+=40, posy, rad, Color.BROWN);
             board[i] = c;
-            circlename.getChildren().add(c);
+            getChildren().add(c);
         }
         
         //ninth row
@@ -195,13 +173,13 @@ public class GUI extends Application {
         for (int i = 56; i < 61; i++) {
             Circle c = new Circle(tileshift+=40, posy, rad, Color.BROWN);
             board[i] = c;
-            circlename.getChildren().add(c);
+            getChildren().add(c);
         }
-        return circlename;
+       
     }
     
-    public Group standardLayout() {
-        Group standard = new Group();
+    public void standardLayout() {
+        //Group standard = new Group();
         
         //white pieces
         //top row
@@ -213,7 +191,8 @@ public class GUI extends Application {
         {
             Circle c = new Circle(tileshift+=40, posy, rad, Color.WHITE);
             pieces[i] = c;
-            standard.getChildren().add(c);
+            getChildren().add(c);
+            //add(c, tileshift+=41, posy);
         }
         
         //second row
@@ -224,7 +203,7 @@ public class GUI extends Application {
         for (int i = 5; i < 11; i++) {
             Circle c = new Circle(tileshift+=40, posy, rad, Color.WHITE);
             pieces[i] = c;
-            standard.getChildren().add(c);
+            getChildren().add(c);
         }
             
         //third row
@@ -235,7 +214,7 @@ public class GUI extends Application {
         for (int i = 13; i < 16; i++) {
             Circle c = new Circle(tileshift+=40, posy, rad, Color.WHITE);
             pieces[i] = c;
-            standard.getChildren().add(c);
+            getChildren().add(c);
         }
         
         //black
@@ -247,7 +226,7 @@ public class GUI extends Application {
         for (int i = 45; i < 48; i++) {
             Circle c = new Circle(tileshift+=40, posy, rad, Color.BLACK);
             pieces[i] = c;
-            standard.getChildren().add(c);
+            getChildren().add(c);
         }
         
         //eighth row
@@ -258,7 +237,7 @@ public class GUI extends Application {
         for (int i = 50; i < 56; i++) {
             Circle c = new Circle(tileshift+=40, posy, rad, Color.BLACK);
             pieces[i] = c;
-            standard.getChildren().add(c);
+            getChildren().add(c);
         }
         
         //ninth row
@@ -269,18 +248,12 @@ public class GUI extends Application {
         for (int i = 56; i < 61; i++) {
             Circle c = new Circle(tileshift+=40, posy, rad, Color.BLACK);
             pieces[i] = c;
-            standard.getChildren().add(c);
+            getChildren().add(c);
         }
         
-        return standard;
+     
     }
-*/
-    /**
-     * Drives the program.
-     * @param args unused
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
+
+  
 
 }
