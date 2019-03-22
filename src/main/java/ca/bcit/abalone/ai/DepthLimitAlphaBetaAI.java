aa25922ea0c18b5914208a026a0db36492ab4b93
Rongyi Chen <kenchenrong@gmail.com>
2019-03-21 23:50:53 -0700

    implemented multi-threading searches

--- src/main/java/ca/bcit/abalone/ai/DepthLimitAlphaBetaAI.java
+++ src/main/java/ca/bcit/abalone/ai/DepthLimitAlphaBetaAI.java
@@ -2,57 +2,84 @@ package ca.bcit.abalone.ai;
 
 import ca.bcit.abalone.game.Game;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class DepthLimitAlphaBetaAI<P, S, A> {
 
+    private ExecutorService threadPoolExecutor;
     private int maxLevel;
     private boolean terminate;
+    private int alpha;
+    private int beta;
+    private int value;
+    private A action;
 
     public A play(Game<P, S, A> game, int maxLevel) {
         this.maxLevel = maxLevel;
+        threadPoolExecutor = Executors.newFixedThreadPool(8);
+        alpha = Integer.MIN_VALUE;
+        beta = Integer.MAX_VALUE;
+        action = null;
 
         return game.isPlayerMax(game.getPlayer())
-                ? maxAction(game, Integer.MIN_VALUE, Integer.MAX_VALUE)
-                : minAction(game, Integer.MIN_VALUE, Integer.MAX_VALUE);
+                ? maxAction(game)
+                : minAction(game);
     }
 
-    private A maxAction(Game<P, S, A> game, int alpha, int beta) {
+    private A maxAction(Game<P, S, A> game) {
         if (game.isTerminal()) {
             return null;
         }
-        int value = Integer.MIN_VALUE;
-        A action = null;
+        value = Integer.MIN_VALUE;
         long time = System.currentTimeMillis();
         for (A a : game.actions()) {
-            int result = minValue(game.result(a), alpha, beta, 1);
-            if (result > value) {
-                value = result;
-                action = a;
-            }
-            alpha = Math.max(alpha, value);
+            threadPoolExecutor.execute(() -> {
+                int result = minValue(game.result(a), alpha, beta, 1);
+                if (result > value) {
+                    value = result;
+                    action = a;
+                }
+                alpha = Math.max(alpha, value);
+            });
+        }
+        threadPoolExecutor.shutdown();
+        try {
+            threadPoolExecutor.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            System.out.println("Level " + maxLevel + " Search Terminated");
         }
         time = System.currentTimeMillis() - time;
-        System.out.println("Search completed in " + time + " ms");
+        System.out.println("Search completed in " + time + " ms, " + "heuristic: " + value);
         return action;
     }
 
-    private A minAction(Game<P, S, A> game, int alpha, int beta) {
+    private A minAction(Game<P, S, A> game) {
         if (game.isTerminal()) {
             return null;
         }
-        int value = Integer.MAX_VALUE;
-        A action = null;
+        value = Integer.MAX_VALUE;
         long time = System.currentTimeMillis();
         for (A a : game.actions()) {
-            int result = maxValue(game.result(a), alpha, beta, 1);
-            if (result < value) {
-                value = result;
-                action = a;
-            }
-            beta = Math.min(beta, value);
+            threadPoolExecutor.execute(() -> {
+                int result = maxValue(game.result(a), alpha, beta, 1);
+                if (result < value) {
+                    value = result;
+                    action = a;
+                }
+                beta = Math.min(beta, value);
 //            System.out.println(++numberOfDone + "/" + game.actions().length);
+            });
+        }
+        threadPoolExecutor.shutdown();
+        try {
+            threadPoolExecutor.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            System.out.println("Level " + maxLevel + " Search Terminated");
         }
         time = System.currentTimeMillis() - time;
-        System.out.println("Search completed in " + time + " ms");
+        System.out.println("Search completed in " + time + " ms, " + "heuristic: " + value);
         return action;
     }
 
