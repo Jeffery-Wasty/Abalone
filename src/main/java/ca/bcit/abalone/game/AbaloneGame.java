1474137f4454658b852bb1f79e46c56dfce25ec4
Rongyi Chen <kenchenrong@gmail.com>
2019-03-25 23:21:35 -0700

    add pruning placeholder

--- src/main/java/ca/bcit/abalone/game/AbaloneGame.java
+++ src/main/java/ca/bcit/abalone/game/AbaloneGame.java
@@ -143,19 +143,35 @@ public class AbaloneGame extends Game<Character, AbaloneGame.State, AbaloneGame.
     // Enumerate all the 6 directions for each marbles on the board
     public AbaloneGame.Action[] actions(AbaloneGame.State state) {
         ArrayList<AbaloneGame.Action> validActions = new ArrayList<>();
+//        if (this.state.turn == 1) {
+//            for (byte[] row : LINEAR_LOCATION) {
+//                for (int i = 0; i < row.length / 2; i++) {
+//                    int loc = row[i];
+//                    addMarbleActions(loc, validActions);
+//                }
+//            }
+//        } else {
         for (int loc = 0; loc < state.board.length; loc++) {
-            for (int i = 1; i <= 3; i++) {
-                for (int j = 0; j < 6; j++) {
-                    AbaloneAction action = new AbaloneAction(i, loc, j);
-                    AbaloneGame.Action gameAction = isValidAction(action);
-                    if (gameAction != null) {
-                        validActions.add(gameAction);
-                    }
+            addMarbleActions(loc, validActions);
+        }
+//        }
+        Collections.shuffle(validActions);
+        return validActions.toArray(new AbaloneGame.Action[0]);
+    }
+
+    private void addMarbleActions(int loc, ArrayList<Action> validActions) {
+        if (loc < 0 || loc >= 61) {
+            return;
+        }
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 0; j < 6; j++) {
+                AbaloneAction action = new AbaloneAction(i, loc, j);
+                AbaloneGame.Action gameAction = isValidAction(action);
+                if (gameAction != null) {
+                    validActions.add(gameAction);
                 }
             }
         }
-        Collections.shuffle(validActions);
-        return validActions.toArray(new AbaloneGame.Action[0]);
     }
 
     @Override
